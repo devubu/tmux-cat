892bded10fdbad7b8dd8082a6fd841ac002c9193
devubu <159164402+devubu@users.noreply.github.com>
2024-06-13 00:08:06 -0400

    Update date_time.sh

--- status/date_time.sh
+++ status/date_time.sh
@@ -4,7 +4,8 @@ show_date_time() {
   index=$1
   icon="$(get_tmux_option "@catppuccin_date_time_icon" "󰃰")"
   color="$(get_tmux_option "@catppuccin_date_time_color" "$thm_blue")"
-  text="$(get_tmux_option "@catppuccin_date_time_text" "%Y-%m-%d %H:%M")"
+  # text="$(get_tmux_option "@catppuccin_date_time_text" "%Y-%m-%d %H:%M")"
+  text="$(get_tmux_option "@catppuccin_date_time_text" "%H:%M %Y/%m/%d")"
 
   module=$(build_status_module "$index" "$icon" "$color" "$text")
 
